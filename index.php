17fb86260d109933b71bfdd06f7e2a331bbd7ce5
ElyldorLeans <elyldorleans@gmail.com>
2018-05-13 16:24:16 +0200

    Files

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require_once('inc/autoload.inc.php');
 $webpage = new Webpage("Eternight - Accueil");
 
 $webpage->appendContent(<<<HTML
-        <h1>TEST</h1>"
+        <h1>TEST</h1>
+        <p>A MODIFIER</p>
 HTML
     );
 
