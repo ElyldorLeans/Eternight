242a64fa8b760c2bd105eedb721e8f1f98b03b92
Aokamii <martin.spaety@gmail.com>
2018-06-07 15:38:38 +0200

    Create erreur_403.php

---
+++ erreurs/erreur_403.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('inc/autoload.inc.php');
+
+$webpage = new Webpage("Eternight - 403");
+
+$webpage->appendContent(<<<HTML
+<div class="bs-component">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            T'as rien à faire là, retourne à l'accueil.
+        </div>
+    </nav>
+</div>
+HTML
+);
+
+
+
+
+
+echo($webpage->toHTML());
